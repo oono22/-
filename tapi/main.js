3bac848e59ae8491d4195e22c5b5fba422bd89ee
KaituXD <bhka1270334@gn.iwasaki.ac.jp>
2020-07-22 15:01:45 +0900

    むりぽ

--- tapi/main.js
+++ tapi/main.js
@@ -1,11 +1,12 @@
 window.onload=function(){
-	var tapi_cnt = document.getElementById('tapimg');
-	var tpcnt = document.getElementById('tpcnt');
+	var tapi_cnt=document.getElementById('tapimg');
+	var tpcnt=document.getElementById('tpcnt');
 	//var a=document.getElementById('tpcnt');
-	var cnt_value = 0;
+	var cnt_value=0,fl_num=20,fl_cnt=0,fl_x=0,fl_y=600,cnt=4,fl_end=0;
 	var item1=0,item2=0,item3=0,item4=0,a=0,shnum=0;
 	var imuch1=15,imuch2=100,imuch3=1100,imuch4=12000;
 	var icps1=1,icps2=8,icps3=20,icps4=47,scps=0;
+
 
 	tapi_cnt.onclick=function(){
 		cnt_value += 1;
@@ -80,6 +81,11 @@ window.onload=function(){
 	var addPcnt=function(){
 		cnt_value+=scps;
 		document.getElementById('tpcnt').innerHTML=cnt_value;
+		if (cnt_value>=fl_num && fl_end==0) {
+			fl_cnt+=1;
+			fl_num+=2;
+			tpflag();
+		}
 	};
 
 	setInterval(addPcnt,1000);
@@ -101,9 +107,31 @@ window.onload=function(){
 	        	if (posY >= imgWidth) posY= 0;
 	        	//scrollSpeed分移動
 	        	posY += scrollSpeed;
-	  			$('.tapioca').css("background-position","10px "+posY+"px");
+	  			$('.tapioca').css("background-position","50% "+posY+"px");
 	    	}, 1);
 		});
 	};
 
+	function tpflag(){
+		var img = document.createElement('img');
+		img.src = './image/maru4.png';
+		img.id=fl_cnt;
+		//$(function(){$('img').attr("id",fl_cnt);});
+		document.getElementById('fl_tp').appendChild(img);
+		$('#'+fl_cnt).css("width","120px");
+		$('#'+fl_cnt).css("height","120px");
+		$('#'+fl_cnt).css("position","absolute");
+		$('#'+fl_cnt).css("transform","translate("+fl_x+"%, "+fl_y+"%)");
+	    fl_x+=100;
+	    if (fl_cnt==cnt) {
+	    	fl_y-=100;
+	    	fl_x=0;
+	    	cnt+=4;
+	    }
+	    	if (fl_cnt==29) {fl_end=1;
+	    }
+		if (fl_end==1) {
+			alert('flag=---------');
+		}
+	};
 };
