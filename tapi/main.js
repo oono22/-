82e6522da7d58dbba7909848c51ae6e1eebc835d
KaituXD <bhka1270334@gn.iwasaki.ac.jp>
2020-08-15 00:59:43 +0900

    最新

--- tapi/main.js
+++ tapi/main.js
@@ -1,33 +1,38 @@
 window.onload=function(){
+
+	//変数の定義
 	var tapi_cnt=document.getElementById('tapimg');
 	var tpcnt=document.getElementById('tpcnt');
-	//var a=document.getElementById('tpcnt');
-	var cnt_value=0,fl_num=20,fl_cnt=0,fl_x=0,fl_y=600,cnt=4,fl_end=0;
+	var ow=(window.screen.width/2-275)/4,oh=window.screen.height-240,fl_oh=Math.ceil((oh+120)/120);
+	var cnt_value=0,fl_num=100,fl_cnt=0,fl_end=0,fl_y=oh,cnt=4;
 	var item1=0,item2=0,item3=0,item4=0,a=0,shnum=0;
 	var imuch1=15,imuch2=100,imuch3=1100,imuch4=12000;
-	var icps1=1,icps2=8,icps3=20,icps4=47,scps=0;
+	var icps1=1,icps2=8,icps3=20,icps4=47,i_cps=0;
+	var gg=0,wp=fl_oh;
 
-
+	//タピオカをクリックしたときに１つタピオカを増やす
 	tapi_cnt.onclick=function(){
 		cnt_value += 1;
 		tpcnt.innerHTML=cnt_value;
 	};
 
+	//タピオカをクリックしたときに小さくする
 	tapi_cnt.onmousedown=function(){
 		tapi_cnt.style.width="280px";
 		tapi_cnt.style.height="280px";
 	};
-
+	//タピオカをクリックしたときに大きくする
 	tapi_cnt.onmouseup=function(){
 		tapi_cnt.style.width="300px";
 		tapi_cnt.style.height="300px";
 	};
 
+	//アイテムをクリックしたときの処理
 	document.getElementById("item1").onclick = function(){
 		if (cnt_value>=imuch1) {
 			item1+=1;
 			cnt_value-=Math.floor(imuch1);
-			scps+=icps1;
+			i_cps+=icps1;
 			imuch1*=1.15;
 			document.getElementById('tpcnt').innerHTML=cnt_value;
 			document.getElementById("i_num1").innerHTML=item1;
@@ -35,11 +40,12 @@ window.onload=function(){
 		};	
 	};
 
+	//アイテムをクリックしたときの処理
 	document.getElementById("item2").onclick = function(){
 		if (cnt_value>=imuch2) {
 			item2+=1;
 			cnt_value-=Math.floor(imuch2);
-			scps+=icps2;
+			i_cps+=icps2;
 			imuch2*=1.15;
 			document.getElementById('tpcnt').innerHTML=cnt_value;
 			document.getElementById("i_num2").innerHTML=item2;
@@ -47,11 +53,12 @@ window.onload=function(){
 		};	
 	};
 
+	//アイテムをクリックしたときの処理
 	document.getElementById("item3").onclick = function(){
 		if (cnt_value>=imuch3) {
 			item3+=1;
 			cnt_value-=Math.floor(imuch3);
-			scps+=icps3;
+			i_cps+=icps3;
 			imuch3*=1.15;
 			document.getElementById('tpcnt').innerHTML=cnt_value;
 			document.getElementById("i_num3").innerHTML=item3;
@@ -59,11 +66,12 @@ window.onload=function(){
 		};	
 	};
 
+	//アイテムをクリックしたときの処理
 	document.getElementById("item4").onclick = function(){
 		if (cnt_value>=imuch4) {
 			item4+=1;
 			cnt_value-=Math.floor(imuch4);
-			scps+=icps4;
+			i_cps+=icps4;
 			imuch4*=1.15;
 			document.getElementById('tpcnt').innerHTML=cnt_value;
 			document.getElementById("i_num4").innerHTML=item4;
@@ -71,26 +79,26 @@ window.onload=function(){
 		};	
 	};
 
+	//タピオカループ処理
 	var parsecond=function(){
-		document.getElementById('i_cps').innerHTML=scps;
-		if (shnum==0 && scps>=1) {
+		document.getElementById('i_cps').innerHTML=i_cps;
+		if (shnum==0 && i_cps>=50) {
 			shower();
 		}
 	};
-
 	var addPcnt=function(){
-		cnt_value+=scps;
+		cnt_value+=i_cps;
 		document.getElementById('tpcnt').innerHTML=cnt_value;
 		if (cnt_value>=fl_num && fl_end==0) {
 			fl_cnt+=1;
-			fl_num+=2;
+			fl_num+=(fl_cnt*100);
 			tpflag();
 		}
 	};
-
 	setInterval(addPcnt,1000);
 	setInterval(parsecond,50);
 
+	//タピオカシャワーの処理
 	function shower(){
 		$(document).ready(function () {
 			shnum+=1;
@@ -112,26 +120,26 @@ window.onload=function(){
 		});
 	};
 
+	//タピオカを出現させる処理
 	function tpflag(){
 		var img = document.createElement('img');
 		img.src = './image/maru4.png';
 		img.id=fl_cnt;
-		//$(function(){$('img').attr("id",fl_cnt);});
 		document.getElementById('fl_tp').appendChild(img);
 		$('#'+fl_cnt).css("width","120px");
 		$('#'+fl_cnt).css("height","120px");
 		$('#'+fl_cnt).css("position","absolute");
-		$('#'+fl_cnt).css("transform","translate("+fl_x+"%, "+fl_y+"%)");
-	    fl_x+=100;
+		$('#'+fl_cnt).css("transform","translate("+gg+"px, "+fl_y+"px)");
+	    gg+=ow;
 	    if (fl_cnt==cnt) {
-	    	fl_y-=100;
-	    	fl_x=0;
+	    	fl_y-=120;
+	    	gg=0;
 	    	cnt+=4;
 	    }
-	    	if (fl_cnt==29) {fl_end=1;
+	    	if (fl_cnt>wp*4) {fl_end=1;
 	    }
 		if (fl_end==1) {
-			alert('flag=---------');
+			alert('flag=you are tapioka master');
 		}
 	};
 };
